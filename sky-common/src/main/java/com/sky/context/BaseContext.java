3c5f7ed88563eb2573eab92f43a8cb4ac2baec3b
Andy-Hy <934052056@qq.com>
2025-03-12 20:36:24 +0800

    新增员工2

--- sky-common/src/main/java/com/sky/context/BaseContext.java
+++ sky-common/src/main/java/com/sky/context/BaseContext.java
@@ -1,19 +1,19 @@
 package com.sky.context;
 
+//封装ThreadLocal方法
 public class BaseContext {
 
     public static ThreadLocal<Long> threadLocal = new ThreadLocal<>();
-
     public static void setCurrentId(Long id) {
-        threadLocal.set(id);
+        threadLocal.set(id);    //设置当前线程的局部变量值
     }
 
     public static Long getCurrentId() {
-        return threadLocal.get();
+        return threadLocal.get();   //返回当前线程对应的局部变量值
     }
 
     public static void removeCurrentId() {
-        threadLocal.remove();
+        threadLocal.remove();   //移除当前线程的局部变量值
     }
 
 }
